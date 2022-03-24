630985ed192ea5ced29f76a32d0040099c68c6ae
NAiveD <nice-die@live.com>
2022-03-24 17:03:28 +0800

    feat: 侧拉抽屉

--- app/src/main/java/cn/svecri/feedive/data/FeedInGroupDao.kt
+++ app/src/main/java/cn/svecri/feedive/data/FeedInGroupDao.kt
@@ -3,6 +3,7 @@ package cn.svecri.feedive.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FeedInGroupDao {
@@ -11,4 +12,9 @@ interface FeedInGroupDao {
 
     @Query("select * from feed_in_group")
     fun getAll(): List<FeedInGroup>
+
+    @Query("select feed.* from feed_in_group " +
+                "left join feed on feed_in_group.feed_id = feed.feed_id " +
+                "where feed_in_group.feed_group_id = :groupId")
+    fun getGroupChildren(groupId: Int): Flow<List<Feed>>
 }
