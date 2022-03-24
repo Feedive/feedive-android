9e2339ca68bf1213227d86fe0ce4c081c50e89eb
NAiveD <nice-die@live.com>
2022-03-25 01:27:52 +0800

    fix: 啊这，actions咋没触发

--- app/src/main/java/cn/svecri/feedive/data/FeedInGroupDao.kt
+++ app/src/main/java/cn/svecri/feedive/data/FeedInGroupDao.kt
@@ -13,8 +13,10 @@ interface FeedInGroupDao {
     @Query("select * from feed_in_group")
     fun getAll(): List<FeedInGroup>
 
-    @Query("select feed.* from feed_in_group " +
+    @Query(
+        "select feed.* from feed_in_group " +
                 "left join feed on feed_in_group.feed_id = feed.feed_id " +
-                "where feed_in_group.feed_group_id = :groupId")
+                "where feed_in_group.feed_group_id = :groupId"
+    )
     fun getGroupChildren(groupId: Int): Flow<List<Feed>>
 }
