e15fba4e58798fcbbe6a744ff2ff7ad9189c9ae5
huanglin <simplelin00@outlook.com>
2022-04-19 11:20:51 +0800

    ui: change color

--- app/src/main/java/cn/svecri/feedive/ui/theme/Color.kt
+++ app/src/main/java/cn/svecri/feedive/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package cn.svecri.feedive.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(0xFFCE93D8)
+val Purple500 = Color(0xFF9C27B0)
+val Purple700 = Color(0xFF7B1FA2)
+val Teal200 = Color(0xFFF48FB1)
